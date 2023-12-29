2c85aad75f77c03786d1968db4c4d9d9eec83ca2
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-12-29 09:23:51 -0300

    Add files via upload

---
+++ Search Engine/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace SearchEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.campotextoPesquisa = new System.Windows.Forms.TextBox();
+            this.comboBoxopcaoTabela = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPesquisar = new System.Windows.Forms.Button();
+            this.dataGridViewResultados = new System.Windows.Forms.DataGridView();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pesquisa: ";
+            // 
+            // campotextoPesquisa
+            // 
+            this.campotextoPesquisa.Location = new System.Drawing.Point(15, 414);
+            this.campotextoPesquisa.Name = "campotextoPesquisa";
+            this.campotextoPesquisa.Size = new System.Drawing.Size(544, 20);
+            this.campotextoPesquisa.TabIndex = 1;
+            // 
+            // comboBoxopcaoTabela
+            // 
+            this.comboBoxopcaoTabela.FormattingEnabled = true;
+            this.comboBoxopcaoTabela.Items.AddRange(new object[] {
+            "Aluno",
+            "Disciplina",
+            "Turma"});
+            this.comboBoxopcaoTabela.Location = new System.Drawing.Point(569, 413);
+            this.comboBoxopcaoTabela.Name = "comboBoxopcaoTabela";
+            this.comboBoxopcaoTabela.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxopcaoTabela.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(566, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Área da Pesquisa";
+            // 
+            // buttonPesquisar
+            // 
+            this.buttonPesquisar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(156)))), ((int)(((byte)(154)))));
+            this.buttonPesquisar.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPesquisar.ForeColor = System.Drawing.Color.White;
+            this.buttonPesquisar.Location = new System.Drawing.Point(706, 402);
+            this.buttonPesquisar.Name = "buttonPesquisar";
+            this.buttonPesquisar.Size = new System.Drawing.Size(82, 34);
+            this.buttonPesquisar.TabIndex = 4;
+            this.buttonPesquisar.Text = "Pesquisar";
+            this.buttonPesquisar.UseVisualStyleBackColor = false;
+            this.buttonPesquisar.Click += new System.EventHandler(this.buttonPesquisar_Click);
+            // 
+            // dataGridViewResultados
+            // 
+            this.dataGridViewResultados.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(156)))), ((int)(((byte)(154)))));
+            this.dataGridViewResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResultados.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridViewResultados.Location = new System.Drawing.Point(12, 45);
+            this.dataGridViewResultados.Name = "dataGridViewResultados";
+            this.dataGridViewResultados.Size = new System.Drawing.Size(773, 313);
+            this.dataGridViewResultados.TabIndex = 5;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(282, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(237, 26);
+            this.labelTitulo.TabIndex = 6;
+            this.labelTitulo.Text = "Aplicativo de Pesquisa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(205)))), ((int)(((byte)(169)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.dataGridViewResultados);
+            this.Controls.Add(this.buttonPesquisar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxopcaoTabela);
+            this.Controls.Add(this.campotextoPesquisa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox campotextoPesquisa;
+        private System.Windows.Forms.ComboBox comboBoxopcaoTabela;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonPesquisar;
+        private System.Windows.Forms.DataGridView dataGridViewResultados;
+        private System.Windows.Forms.Label labelTitulo;
+    }
+}
+
---
+++ Search Engine/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace SearchEngine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string tabelaSelecionada = comboBoxopcaoTabela.SelectedItem.ToString();
+                string campoPesquisa = campotextoPesquisa.Text;
+
+                string query = string.Empty;
+
+                using (MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString))
+                {
+                    connection.Open();
+
+                    switch (tabelaSelecionada)
+                    {
+                        case "Aluno":
+
+                            query = "SELECT * FROM Aluno WHERE Nome LIKE @param OR CPF LIKE @param";
+                            break;
+
+                        case "Disciplina":
+
+                            query = "SELECT * FROM Disciplinas WHERE NomeDisciplina LIKE @param OR CodigoDisciplina LIKE @param";
+                            break;
+
+                        case "Turma":
+
+                            query = "SELECT * FROM Turma_Aluno WHERE NumeroTurma LIKE @param";
+                            break;
+
+                        default:
+                            MessageBox.Show("Opção de tabela inválida");
+                            return;
+                    }
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@param", "%" + campoPesquisa + "%");
+
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+
+                            // Exiba os resultados no DataGridView
+                            dataGridViewResultados.DataSource = dataTable;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
